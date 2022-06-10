268a2fb6de8eb57a047adc1d95f4e4827fb48692
SmogunovAV <smogunov@morion.ru>
2022-06-10 16:28:49 +0500

    Strange git..

--- app/src/main/java/com/smogunovandrey/tasksplanning/MainActivity.kt
+++ app/src/main/java/com/smogunovandrey/tasksplanning/MainActivity.kt
@@ -2,10 +2,25 @@ package com.smogunovandrey.tasksplanning
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.DocumentsContract
+import androidx.room.Room
+import com.smogunovandrey.tasksplanning.db.AppDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private val scope: CoroutineScope = CoroutineScope(Dispatchers.Default)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val db = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "tasks.db").build()
+        scope.launch {
+            db.mainDao().allTask()
+        }
+
     }
 }
--- app/src/main/java/com/smogunovandrey/tasksplanning/db/tables.kt
+++ app/src/main/java/com/smogunovandrey/tasksplanning/db/tables.kt
@@ -1,4 +1,77 @@
 package com.smogunovandrey.tasksplanning.db
 
-class tables {
+import androidx.room.*
+
+/**
+ * Type trigger
+ */
+enum class TriggerType{
+    HAND, GPS_IN, GPS_OUT, NFC, VOICE, BTN_HEADPHONE
 }
+
+/**
+ * Trigger for event
+ */
+@Entity
+data class Trigger(
+    @PrimaryKey(autoGenerate = true) val id: Long,
+    val type: TriggerType
+)
+
+/**
+ * Point for event
+ */
+@Entity
+data class Point(
+    @PrimaryKey(autoGenerate = true) val id: Long,
+    val name: String,
+    val triggerType: TriggerType
+)
+
+/**
+ * Additional params for points with GPS trigger
+ */
+@Entity
+data class PointGps(
+    val idPoint: Long,
+    val latitude: Float,
+    val longitude: Float
+)
+
+/**
+ * Task item for persist database
+ */
+
+@Entity
+data class Task(
+    @PrimaryKey(autoGenerate = true) val id: Long,
+    val name: String
+)
+
+@Entity(foreignKeys = [ForeignKey(entity = Task::class, parentColumns = arrayOf("id"),
+                                    childColumns = arrayOf("idTask"),
+                                    onDelete = ForeignKey.CASCADE),
+                      ForeignKey(entity =  Point::class, parentColumns = ["id"],
+                                    childColumns = ["idPoint"],
+                                    onDelete = ForeignKey.CASCADE)]
+)
+data class TaskPoint(
+    @PrimaryKey(autoGenerate = true) val id: Long,
+    val idTask: Long,
+    val idPoint: Long
+)
+
+
+
+@Dao
+interface MainDao{
+    @Query("select * FROM TaskPoint")
+//    fun allTask(): Map<Task, List<Point>>
+    fun  allTask(): List<TaskPoint>
+}
+
+@Database(entities = [Trigger::class, Point::class, Task::class, TaskPoint::class], version = 1)
+abstract class AppDatabase: RoomDatabase(){
+    abstract fun mainDao(): MainDao
+}
+
