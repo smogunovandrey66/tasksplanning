125186fb4befb1a03d2a69194daffc8154916e73
SmogunovAV <smogunov@morion.ru>
2022-06-14 16:57:05 +0500

    Choice suspend for DAO one-shot operation (also may be Flow, LiveData)

--- app/src/main/java/com/smogunovandrey/tasksplanning/MainActivity.kt
+++ app/src/main/java/com/smogunovandrey/tasksplanning/MainActivity.kt
@@ -1,17 +1,17 @@
 package com.smogunovandrey.tasksplanning
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.DocumentsContract
+import androidx.appcompat.app.AppCompatActivity
 import androidx.room.Room
 import com.smogunovandrey.tasksplanning.db.AppDatabase
+import com.smogunovandrey.tasksplanning.db.Point
+import com.smogunovandrey.tasksplanning.db.TriggerType
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
-    private val scope: CoroutineScope = CoroutineScope(Dispatchers.Default)
+    private val scope: CoroutineScope = CoroutineScope(Dispatchers.Main)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +19,12 @@ class MainActivity : AppCompatActivity() {
 
         val db = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "tasks.db").build()
         scope.launch {
-            db.mainDao().allTask()
+        db.mainDao().allTaskWithPoint()
+
+//            for(triggerType in TriggerType.values()){
+//                val point: Point = Point(0, "name ${triggerType.name}", triggerType)
+//                AppDatabase.getInstance(applicationContext).mainDao().insertPoint(point)
+//            }
         }
 
     }
--- app/src/main/java/com/smogunovandrey/tasksplanning/db/tables.kt
+++ app/src/main/java/com/smogunovandrey/tasksplanning/db/tables.kt
@@ -1,6 +1,9 @@
 package com.smogunovandrey.tasksplanning.db
 
+import android.content.Context
+import android.util.Log
 import androidx.room.*
+import java.util.*
 
 /**
  * Type trigger
@@ -12,18 +15,18 @@ enum class TriggerType{
 /**
  * Trigger for event
  */
-@Entity
-data class Trigger(
-    @PrimaryKey(autoGenerate = true) val id: Long,
-    val type: TriggerType
-)
+//@Entity
+//data class Trigger(
+//    @PrimaryKey(autoGenerate = true) val id: Long,
+//    val type: TriggerType
+//)
 
 /**
  * Point for event
  */
 @Entity
 data class Point(
-    @PrimaryKey(autoGenerate = true) val id: Long,
+    @PrimaryKey(autoGenerate = true) val idPoint: Long,
     val name: String,
     val triggerType: TriggerType
 )
@@ -33,7 +36,7 @@ data class Point(
  */
 @Entity
 data class PointGps(
-    val idPoint: Long,
+    @PrimaryKey val idPoint: Long,
     val latitude: Float,
     val longitude: Float
 )
@@ -44,34 +47,108 @@ data class PointGps(
 
 @Entity
 data class Task(
-    @PrimaryKey(autoGenerate = true) val id: Long,
+    @PrimaryKey(autoGenerate = true) val idTask: Long,
     val name: String
 )
 
-@Entity(foreignKeys = [ForeignKey(entity = Task::class, parentColumns = arrayOf("id"),
-                                    childColumns = arrayOf("idTask"),
-                                    onDelete = ForeignKey.CASCADE),
-                      ForeignKey(entity =  Point::class, parentColumns = ["id"],
-                                    childColumns = ["idPoint"],
-                                    onDelete = ForeignKey.CASCADE)]
-)
-data class TaskPoint(
-    @PrimaryKey(autoGenerate = true) val id: Long,
+@Entity(primaryKeys = ["idTask", "idPoint"])
+data class TaskPointCrossRef(
     val idTask: Long,
     val idPoint: Long
 )
 
+data class TaskWithPoint(
+    @Embedded val task: Task,
 
+    @Relation(parentColumn = "idTask", entityColumn = "idPoint", associateBy = Junction(TaskPointCrossRef::class))
+    val listTaskPoint: List<Point>
+)
+
+@Entity(primaryKeys = ["idTask", "idPoint"])
+data class RunTaskCrossRef(
+    val idTask: Long,
+    val idPoint: Long,
+    val num: Long,
+    val dateBegin: Date = Date(),
+    val dateEnd: Date = Date()
+)
+
+data class RunTaskWithPoint(
+    @Embedded val task: Task,
+
+    @Relation(parentColumn = "idTask", entityColumn = "idPoint", associateBy = Junction(RunTaskCrossRef::class))
+    val listPoints: List<Point>
+)
 
 @Dao
 interface MainDao{
-    @Query("select * FROM TaskPoint")
-//    fun allTask(): Map<Task, List<Point>>
-    fun  allTask(): List<TaskPoint>
+    //Task
+    @Insert
+    suspend fun addTask(task: Task)
+
+    @Delete
+    suspend fun deleteTask(task: Task)
+
+    @Update
+    suspend fun updateTask(task: Task)
+
+    //Point
+    @Insert
+    suspend fun insertPoint(point: Point)
+
+    @Delete
+    suspend fun deletePoint(point: Point)
+
+    @Update
+    suspend fun updatePoint(point: Point)
+
+    //TaskPoint
+    @Insert
+    suspend fun insertTaskPoint(taskPoint: TaskPointCrossRef)
+
+    @Delete
+    suspend fun deleteTaskPoint(taskPoint: TaskPointCrossRef)
+
+    @Query("select * from Task where idTask = :takId")
+    suspend fun pointsByTask(takId: Long): TaskWithPoint?
+
+    @Query("select * FROM Task")
+    suspend fun  allTaskWithPoint(): List<TaskWithPoint>
 }
 
-@Database(entities = [Trigger::class, Point::class, Task::class, TaskPoint::class], version = 1)
-abstract class AppDatabase: RoomDatabase(){
+@Database(entities = [/*Trigger::class*,*/ Point::class, Task::class, TaskPointCrossRef::class], version = 1)
+@TypeConverters(Converters::class)
+abstract class AppDatabase : RoomDatabase(){
     abstract fun mainDao(): MainDao
+
+    companion object{
+        private val DB_NAME = "task.db"
+
+        @Volatile
+        private var instance: AppDatabase? = null
+
+        /**
+         * Singleton  with double-check locking. Thread safe and lazy.
+         */
+        fun getInstance(context: Context): AppDatabase = instance ?: synchronized(this){
+            val res =  instance ?: Room.databaseBuilder(context, AppDatabase::class.java, DB_NAME).build()
+            instance = res
+            res
+        }
+    }
+}
+
+class Converters{
+    @TypeConverter
+    fun fromTimestamp(value: Long?): Date?{
+        return value?.let{
+            Date(it)
+        }
+    }
+
+    @TypeConverter
+    fun dateToTimestamp(value: Date?): Long?{
+        return value?.time
+    }
 }
 
