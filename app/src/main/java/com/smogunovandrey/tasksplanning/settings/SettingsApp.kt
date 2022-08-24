6967f003661464d079e177c5518273040f29a13b
SmogunovAV <smogunov@morion.ru>
2022-08-24 16:34:24 +0500

    Dependency fo Preferences DataStore in build.gradle

---
+++ app/src/main/java/com/smogunovandrey/tasksplanning/settings/SettingsApp.kt
@@ -0,0 +1,8 @@
+package com.smogunovandrey.tasksplanning.settings
+
+import android.content.SharedPreferences
+import android.preference.PreferenceDataStore
+
+class SettingsApp {
+    val sp: SharedPreferences? = null
+}
